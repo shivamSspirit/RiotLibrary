38388f1af71e5f2a935a0de590d96b7ca46e6e32
shivam <shivam.spirite3@outlook.com>
2022-06-25 02:30:55 +0530

    add prexifier

--- src/Routers/routes.js
+++ src/Routers/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AuthRoutes from "./authRoutes";
 
 import Mockman from "mockman-js";
